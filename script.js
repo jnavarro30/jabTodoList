1a49c2787ddb916ba7c6348782b0cfababc3db03
User42 <jnavarro30@live.com>
2020-09-22 12:29:33 -0700

    commit update response and functionallity

--- script.js
+++ script.js
@@ -1,64 +1,81 @@
-
-
 const TodoListApp = (_ => {
 
-    //Variables
-
-    const headerInputEl = document.querySelector('.header__input'),
-          mainListEl = document.querySelector('.main__list'),
-          itemTextEls = document.querySelectorAll('.item__text'),
-          itemDeleteEls = document.querySelectorAll('.item__delete');
+    // variables
+    const todoAppEl = document.querySelector('.todo__app'),
+          headerInputEl = document.querySelector('.header__input'),
+          mainListEl = document.querySelector('.main__list');
 
     const init = _ => {
         listeners();
-    }
+    };
 
     const createItem = text => {
         let newItem = document.createElement('li');
         newItem.classList.add('list__item');
         newItem.innerHTML = `
-        <p class="item__text">${text}</p>
+        <p class="item__text" contenteditable="false">${text}</p>
         <i class="fas fa-times item__delete"></i>
         `;
         return newItem;
-    }
+    };
 
     const render = _ => {
         let newItem = createItem(headerInputEl.value);
                 mainListEl.insertBefore(newItem, mainListEl.children[0]);
                 headerInputEl.value = '';
-    }
+    };
 
     const listeners = _ => {
-        //Input Value
 
+        todoAppEl.addEventListener('click', event => {
+            const allItemEls = document.querySelectorAll('.item__text');
+
+            if(event.target.matches('.item__text')) {
+                let text = event.target.textContent;
+                allItemEls.forEach(item => {
+                    if (text != item.textContent) item.contentEditable = false;
+                });
+            } else allItemEls.forEach(item => item.contentEditable = false);
+        });
+        
+        // input Value
         headerInputEl.addEventListener('keypress', event => {
             if(headerInputEl.value.length > 50 && !/\s/.test(headerInputEl.value)) {
                 headerInputEl.value = '';
             }
             if(event.keyCode == 13 && headerInputEl.value) render();
-        })
+        });
 
-        //Crossout Item
-
+        // crossout Item
         mainListEl.addEventListener('click', event => {
-            if(event.target.classList.contains('item__text')) {
-                event.target.classList.toggle('line__through');
+            if (event.target.matches('.item__text')) {
+                let text = event.target;
+                if (text.contentEditable == 'true') text.classList.remove('line__through');
+                else text.classList.toggle('line__through');
             }
-        })
+        });
 
-        //Delete Item
-
+        // delete Item
         mainListEl.addEventListener('click', event => {
             if(event.target.classList.contains('item__delete')) {
                 event.target.parentElement.remove();
             }
-        })
-    }
+        });
+
+        // edit Item 
+        mainListEl.addEventListener('dblclick', event => {
+            if(event.target.matches('.item__text')) {
+                let textItem = event.target;
+                textItem.contentEditable = true;
+                textItem.classList.remove('line__through');
+            }
+        });
+
+    };
 
     return {
         init
-    }
+    };
 })();
 
 TodoListApp.init();
